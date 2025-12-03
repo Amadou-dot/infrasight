79cc9b9d0ea404f9981e37090e5ef07a60dc5385
Amadou-dot <amadou.sseck@gmail.com>
2025-12-02 20:15:08 -0700

    Remove debug logging from Pusher configuration

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -4,10 +4,6 @@ const appId = process.env.PUSHER_APP_ID;
 const key = process.env.PUSHER_KEY;
 const secret = process.env.PUSHER_SECRET;
 const cluster = process.env.PUSHER_CLUSTER;
-console.log(appId);
-console.log(key);
-console.log(secret);
-console.log(cluster);
 
 if (!appId || !key || !secret || !cluster) {
   throw new Error(
